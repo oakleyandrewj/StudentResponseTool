b8082dac0c9cbcd9a670fd32dfa0e333c94b98ee
Andrew Oakley <oakleyandrewj@gmail.com>
2022-01-14 11:21:05 +0000

    tidy up

--- Student Responses/script.js
+++ Student Responses/script.js
@@ -2,36 +2,29 @@ var green = 0;
 var amber = 0;
 var red = 0;
 var total = 0;
-//import { stat as _stat, appendFile, writeFile } from 'fs';
-//import json2csv from 'json2csv';
 var subject = 'my Subject';
 var body = 'myBody';
 
 function addGreen() {
   green += 1;
-  //console.log(green); 
   ClearResult();
   AddResult('Thank You');
-  //console.log(amber); 
 }
 function addRed() {
   red += 1;  
   ClearResult();
   AddResult('Thank You');
-  //console.log(red); 
 }
 
 function addAmber() {
   amber += 1;  
   ClearResult();
   AddResult('Thank You');
-  //console.log(red); 
 }
 
 function calcTotals() {
   //document.getElementById("myBody").innerHTML = 'green is '+green + ' amber is '+amber+ ' red is '+red; 
   FinishHere();
-  //console.log(total); 
 }
           
 function FadeDiv() {
